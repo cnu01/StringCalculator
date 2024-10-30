65f2d69ab7cb6aa32409916396f92731de71b108
Srinivas Vallala <srinivasvallala01@gmai.com>
2024-10-30 15:31:25 +0530

    Remove console statements for clean output

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -19,20 +19,4 @@ function add(numbers) {
     return numberArray.filter(num => parseInt(num) <= 1000).reduce((sum, num) => sum + parseInt(num), 0);
 }
 
-
-// console.log(add(''));
-// console.log(add('10'));
-// console.log(add('1,5'));
-// console.log(add('1,2,3,4'));
-// console.log(add('1,2\n,3,4'));
-// console.log(add('1\n'))
-// console.log(add("//;\n1;2;5;6;7;\n8;9;\n10"));
-// console.log(add("//|\n2|3|4"));
-// console.log(add("//#\n5#6#7"));
-// console.log(add("1,-2,3,-4"));
-// console.log(add("2,1001,6"));
-// console.log(add("//[***]\n1***2***3"));
-// console.log(add("//[*][%]\n1*2%3"));
-// console.log(add("//[##][%%]\n4##5%%6"));
-
 module.exports = add;
